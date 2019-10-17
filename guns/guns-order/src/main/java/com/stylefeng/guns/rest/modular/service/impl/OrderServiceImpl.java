012c59f34196c60f95cce19ecbb043f5c124e739
xukai-55223 <269147002@qq.com>
2019-10-17 20:09:21 +0800

    xukai

--- guns/guns-order/src/main/java/com/stylefeng/guns/rest/modular/service/impl/OrderServiceImpl.java
+++ guns/guns-order/src/main/java/com/stylefeng/guns/rest/modular/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import com.stylefeng.guns.rest.PageInfoVO;
 import com.stylefeng.guns.rest.common.persistence.dao.MoocOrderTMapper;
 import com.stylefeng.guns.rest.order.OrderService;
 import com.stylefeng.guns.rest.order.bean.OrderInfoVO;
+import com.stylefeng.guns.rest.order.vo.OrderVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -20,5 +21,25 @@ public class OrderServiceImpl implements OrderService {
     public List<OrderInfoVO> getOrderInfo(String userId, PageInfoVO pageInfo) {
         List<OrderInfoVO> orderInfos = orderTMapper.selectOrdersByUsername(userId,pageInfo);
         return orderInfos;
+    }
+
+    @Override
+    public boolean isTrueSeats(String fieldId, String seats) {
+        return false;
+    }
+
+    @Override
+    public boolean isNotSoldSeats(String fieldId, String seats) {
+        return false;
+    }
+
+    @Override
+    public OrderVo saveOrderInfo(Integer fields, String soldSeats, String seatsName, Integer userId) {
+        return null;
+    }
+
+    @Override
+    public List<OrderInfoVO> getOrderInfo(String username) {
+        return null;
     }
 }
