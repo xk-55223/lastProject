8344e93e5bb684bd171c5583c796d04cecc19ec5
xukai-55223 <269147002@qq.com>
2019-10-17 20:40:37 +0800

    xukai

--- guns/guns-api/src/main/java/com/stylefeng/guns/rest/order/OrderService.java
+++ guns/guns-api/src/main/java/com/stylefeng/guns/rest/order/OrderService.java
@@ -1,20 +1,12 @@
 package com.stylefeng.guns.rest.order;
 
-<<<<<<< HEAD
 import com.stylefeng.guns.rest.PageInfoVO;
 import com.stylefeng.guns.rest.order.bean.OrderInfoVO;
-=======
->>>>>>> 5d33411dbc2ef239fc828a3ec5f651d80ce11ff1
-
-import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.rest.order.vo.OrderVo;
-import com.stylefeng.guns.rest.order.bean.OrderInfoVO;
 import java.util.List;
 
 public interface OrderService {
-<<<<<<< HEAD
     List<OrderInfoVO> getOrderInfo(String userId, PageInfoVO pageInfo);
-=======
 
     boolean isTrueSeats(String fieldId,String seats);
 
@@ -25,5 +17,4 @@ public interface OrderService {
 
     List<OrderInfoVO> getOrderInfo(String username);
 
->>>>>>> 5d33411dbc2ef239fc828a3ec5f651d80ce11ff1
 }
